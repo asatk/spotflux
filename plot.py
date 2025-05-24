ba6fef413a06411a8f63bbdda4efec46d37c8870
asatk <asa15@rice.edu>
2025-05-24 14:15:14 -0400

    animate plot

--- plot.py
+++ plot.py
@@ -1,15 +1,16 @@
 import numpy as np
 from matplotlib import pyplot as plt
+import matplotlib.animation as anim
 
 ntheta = 128
 nphi = 256
 
-nsteps = 100000
+nt = 10000001
 dt = 3e1 / 86400    # days
-freq = 10000     # steps
+freq = 2500     # steps
 
 field_rad = 6.957e10
-field_b0 = 8.5 * 1000
+field_b0 = 8.5
 bmr_b0 = 10.0
 bmr_th = np.pi / 3
 bmr_loc = bmr_th / np.pi * ntheta
@@ -28,13 +29,15 @@ field_flux0 = field_b0 * dphi * dth * np.sin(dth)
 
 #a = dS * a
 
-nt = a.shape[0]
+nframes = a.shape[0]
 
 fig, (ax1, ax2) = plt.subplots(ncols=1, nrows=2, sharex=True, sharey=True,
                                figsize=(6, 8))
 im1 = ax1.imshow(a[0], vmin=-field_b0, vmax=field_b0, norm="symlog")
 #im1 = ax1.imshow(a[0], vmin=-field_flux0, vmax=field_flux0, norm="symlog")
 #im1 = ax1.imshow(a[0], vmin=-field_flux0, vmax=field_flux0, norm="linear")
+ax1.axhline(y=bmr_loc, color="orange", ls="--", lw=1.0)
+ax1.axhline(y=ntheta-bmr_loc, color="orange", ls="--", lw=1.0)
 cb1 = plt.colorbar(im1, shrink=0.75, orientation="horizontal", pad=0.2)
 cb1.ax.set_title("Surface Magnetic Field (G)")
 #cb1.ax.set_title("Surface Magnetic Flux (Mx)")
@@ -51,7 +54,8 @@ ax1.set_title("Initial Condition")
 
 im2 = ax2.imshow(a[-1], vmin=-field_b0, vmax=field_b0, norm="symlog")
 #im2 = ax2.imshow(a[-1], vmin=-field_flux0, vmax=field_flux0, norm="symlog")
-ax2.axhline(y=bmr_loc, color="red", ls="--", lw=1.0)
+ax2.axhline(y=bmr_loc, color="orange", ls="--", lw=1.0)
+ax2.axhline(y=ntheta-bmr_loc, color="orange", ls="--", lw=1.0)
 cb2 = plt.colorbar(im2, shrink=0.75, orientation="horizontal", pad=0.2)
 cb2.ax.set_title("Surface Magnetic Field (G)")
 #cb2.ax.set_title("Surface Magnetic Flux (Mx)")
@@ -64,37 +68,60 @@ ax2.set_xticklabels(["0", r"$\pi/2$", r"$\pi$", r"$3\pi/2$", r"$2\pi$"])
 ax2.set_yticks([0, ntheta/2, ntheta-1])
 ax2.set_yticklabels(["0", r"$\pi/2$", r"$\pi$"])
 ax2.set_ylabel(r"Colatitude $\theta$")
-ax2.set_title(f"Final Magnetic Map ({dt * (nt - 1) * freq:.1f} d)")
+ax2.set_title(f"Final Magnetic Map ({dt * (nt - 1) :.1f} d)")
 
 fig.tight_layout()
 
 plt.show()
 
-r"""
-figt, axt = plt.subplots()
-imt = axt.imshow(a[49], vmin=-field_flux0, vmax=field_flux0, norm="symlog")
-axt.axhline(y=bmr_loc, color="red", ls="--", lw=1.0)
-cbt = plt.colorbar(imt, shrink=0.75, orientation="horizontal", pad=0.2)
-cbt.ax.set_title("Surface Magnetic Flux (Mx)")
-cbt.ax.set_xticks([-field_flux0, 0.0, field_flux0])
-axt.set_xlabel(r"Azimuth $\phi$")
-axt.set_xticks([0, nphi//4, nphi/2, 3*nphi//4, nphi-1])
-axt.set_xticklabels(["0", r"$\pi/2$", r"$\pi$", r"$3\pi/2$", r"$2\pi$"])
-axt.set_yticks([0, ntheta/2, ntheta-1])
-axt.set_yticklabels(["0", r"$\pi/2$", r"$\pi$"])
-axt.set_ylabel(r"Colatitude $\theta$")
-axt.set_title(f"Frame 49")
-figt.tight_layout()
-plt.show()
-"""
+figa, (ax1a, ax2a) = plt.subplots(ncols=1, nrows=2, figsize=(10,8))
+ima = ax1a.imshow(a[0], vmin=-field_b0, vmax=field_b0, norm="symlog")
+ax1a.axhline(y=bmr_loc, color="orange", ls="--", lw=1.0)
+ax1a.axhline(y=ntheta-bmr_loc, color="orange", ls="--", lw=1.0)
+cb1a = plt.colorbar(ima, shrink=0.5, orientation="horizontal", pad=0.25)
+cb1a.ax.set_title("Surface Magnetic Field (G)")
+#cb1a.ax.set_title("Surface Magnetic Flux (Mx)")
+cb1a.ax.set_xticks([-field_b0, 0.0, field_b0])
+#cb1a.ax.set_xticks([-field_flux0, 0.0, field_flux0])
+cb1a.ax.xaxis.set_major_formatter("{x:.02e}")
+ax1a.set_xlabel(r"Azimuth $\phi$")
+ax1a.set_xticks([0, nphi//4, nphi/2, 3*nphi//4, nphi-1])
+ax1a.set_xticklabels(["0", r"$\pi/2$", r"$\pi$", r"$3\pi/2$", r"$2\pi$"])
+ax1a.set_yticks([0, ntheta/2, ntheta-1])
+ax1a.set_yticklabels(["0", r"$\pi/2$", r"$\pi$"])
+ax1a.set_ylabel(r"Colatitude $\theta$")
+titlea = ax1a.set_title(f"Surface field evolution step 0 ({0:.1f} d)")
 
+times = np.linspace(0, (nt - 1) * dt, nframes, endpoint=True)
+fluxd = np.sum(a, axis=(1,2))
+linea = ax2a.plot(0, fluxd[0])[0]
+ax2a.set_xlim(xmin=0, xmax=(nt - 1) * dt)
+ax2a.set_xlabel("Time (d)")
+ax2a.set_ylim(ymin=np.min(fluxd), ymax=np.max(fluxd))
+ax2a.set_ylabel("Signed Flux Density (G)")
+ax2a.set_title("Total Signed Magnetic Field (G)")
+
+figa.tight_layout()
+
+def update(t):
+    ima.set_array(a[t])
+    titlea.set_text(f"Surface field evolution step {t} ({dt * t * freq:.1f} d)")
+    linea.set_xdata(times[:t])
+    linea.set_ydata(fluxd[:t])
+    return ima, titlea, linea
+
+ani = anim.FuncAnimation(fig=figa, func=update, frames=nframes, interval=100)
+plt.show()
+
+r"""
 plt.ion()
 plt.figure(1, figsize=(6,4))
 for t in range(nt):
     plt.clf()
     im = plt.imshow(a[t], vmin=-field_b0, vmax=field_b0, norm="symlog")
     #im = plt.imshow(a[t], vmin=-field_flux0, vmax=field_flux0, norm="symlog")
-    plt.axhline(y=bmr_loc, color="red", ls="--", lw=1.0)
+    plt.axhline(y=bmr_loc, color="orange", ls="--", lw=1.0)
+    plt.axhline(y=ntheta-bmr_loc, color="orange", ls="--", lw=1.0)
     cb = plt.colorbar(im, shrink=0.75, orientation="horizontal", pad=0.25)
     cb.ax.set_title("Surface Magnetic Field (G)")
     #cb.ax.set_title("Surface Magnetic Flux (Mx)")
@@ -116,3 +143,4 @@ for t in range(nt):
 
 plt.ioff()
 plt.show()
+"""
